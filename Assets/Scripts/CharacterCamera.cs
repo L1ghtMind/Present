d752b2c0bbfff6b0eb683b67f5b2a73cd017b7f4
Dima <holombik_ak16@nuwm.edu.ua>
2021-03-06 21:11:50 +0200

    Updated character animation and camera script

---
+++ Assets/Scripts/CharacterCamera.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamera : MonoBehaviour
+{
+    // Добавляем объект, за которым будет двигаться камера
+    [Header("GameObject")]
+    // Пишу с нижним подчеркиванием, так gameObject - ключевое слово Unity.
+    // а придумывать новое название переменной мне влом (сорян)
+    public Transform _gameObject;
+
+    [Header("Camera position restrictions")]
+    public float minY;
+    public float maxY;
+    public float minX;
+    public float maxX;
+
+    void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    // Изменяем позицию камеры на экране
+    void UpdateCameraPosition()
+    {
+        try
+        {
+            transform.position = new Vector3(
+                // Положение игрового объекта, за которым мы двигаемся
+                Mathf.Clamp(_gameObject.position.x, minX, maxX),
+                Mathf.Clamp(_gameObject.position.y, minY, maxY),
+                // Положение камеры z должно оставать неизменным 
+                transform.position.z // (если камеры куда-то проваливается, заменить на, например, -10)
+              );
+        }
+        catch (Exception error)
+        {
+            // Ловим ошибку, если по каким то причинам код не может быть выполнен (например, забыли подставить объект в _gameObject)
+            Debug.LogError(error);
+        }
+    }
+}
+
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,16 +5,63 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Animator animator;
+    [Header("Player velocity")]
 
-    // Update is called once per frame
-    void Update()
+    /**
+   ** Ускорение игрока
+   **/
+    // Ось Ox
+    public int xVelocity = 5;
+    // Ось Oy
+    public int yVelocity = 8;
+
+    [SerializeField] private LayerMask ground;
+
+    // Физическое поведение (тело) объекта
+    private Rigidbody2D rigidBody;
+    // Коллайдер, проверка на столкновения
+    private Collider2D coll;
+    public Animator animator;
+    private void Start()
+    {
+        // Получаем доступ к Rigidbody2D объекта Player
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        coll = gameObject.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        updatePlayerPosition();
+    }
+
+    // Обновляем местоположение игрока
+    private void updatePlayerPosition()
     {
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        Vector3 vertical = new Vector3(Input.GetAxis("Vertical"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        // Получаем значение ввода горизонтального перемещение
+        float moveInput = Input.GetAxis("Horizontal");
+        // Получаем значение ввода прыжка
+        float jumpInput = Input.GetAxis("Jump");
+
+        // Значения xVelocity, yVelocity можно задать через инспектор
+
+        if (moveInput < 0)
+        { // Движ влево
+            rigidBody.velocity = new Vector2(-xVelocity, rigidBody.velocity.y);
+        }
+        else if (moveInput > 0)
+        { // Движ вправо
+            rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            rigidBody.velocity = Vector2.zero; // Лично меня дико бесит инерция вбок при приземлении персонажа, отключаем
+        }
+
+        if (jumpInput > 0 && coll.IsTouchingLayers(ground))
+        { //Тип прыгает, если стоит на земле
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, yVelocity);
+        }
     }
 }
