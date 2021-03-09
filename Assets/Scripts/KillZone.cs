abe8b6314a5f06cd72455b4dda437aa8a8483866
Dima <holombik_ak16@nuwm.edu.ua>
2021-03-09 14:51:11 +0200

    Added KillZone prefab

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Damage = 100f;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //If not player then exit
+        if (other.transform.tag=="Player")
+        {
+            EditorSceneManager.LoadScene("SampleScene");
+        }
+        
+    }
+}
