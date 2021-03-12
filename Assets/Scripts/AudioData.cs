76be04b9a85603fc5b4437154ddf46018dc710e8
Dima <holombik_ak16@nuwm.edu.ua>
2021-03-12 13:27:21 +0200

    Added music source

---
+++ Assets/Scripts/AudioData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioData : MonoBehaviour
+{
+    AudioSource audioSource;
+    Scene scene;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "SampleScene")
+        {
+            audioSource.Play();
+        }
+    }
+
+    
+}
